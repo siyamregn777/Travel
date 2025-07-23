f970313e2197adc78c51be09b3b5fb08fc19bd31
siyamregn77 <yeshidagnasiyamregn@gmail.com>
2025-07-23 09:40:01 +0300

    Update dependencies and PostCSS configuration
    - Updated Next.js version to 15.3.5.
    - Updated Tailwind CSS and PostCSS dependencies to their latest versions.
    - Modified PostCSS configuration to use the new plugin structure with Tailwind CSS and Autoprefixer.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
-import type { NextConfig } from "next";
+// import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
 
-export default nextConfig;
+// export default nextConfig;
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true, // ⬅️ This disables image optimization
+  },
+}
+
+module.exports = nextConfig
